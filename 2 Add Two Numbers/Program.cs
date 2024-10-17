a72db9ef71d248c8194041c1cf1b6bb5ce202f50
darkeclipz <lars@rotgers.io>
2024-10-17 16:32:23 +0200

    init

---
+++ 2 Add Two Numbers/Program.cs
@@ -0,0 +1,75 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+
+    private int GetValueOrZero(ListNode n)
+    {
+        if (n == null)
+        {
+            return 0;
+        }
+
+        return n.val;
+    }
+
+    private bool HasNext(ListNode n)
+    {
+        return n != null && n.next != null;
+    }
+
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+
+        var node = new ListNode();
+        var zero = new ListNode();
+        var start = node;
+        int carry = 0;
+
+        while (l1 != null || l2 != null)
+        {
+            node.val = GetValueOrZero(l1) + GetValueOrZero(l2) + carry;
+
+            if (node.val > 9)
+            {
+                carry = 1;
+                node.val -= 10;
+            }
+            else
+            {
+                carry = 0;
+            }
+
+            if (HasNext(l1) || HasNext(l2))
+            {
+                node.next = new ListNode();
+                node = node.next;
+            }
+
+            if (l1 != null)
+            {
+                l1 = l1.next;
+            }
+
+            if (l2 != null)
+            {
+                l2 = l2.next;
+            }
+        }
+
+        if (carry > 0)
+        {
+            node.next = new ListNode();
+            node.next.val = carry;
+        }
+
+        return start;
+    }
+}
---
+++ 670 Maximum Swap/Program.cs
@@ -0,0 +1,25 @@
+﻿var sln = new Solution();
+Console.WriteLine(sln.MaximumSwap(2736));
+Console.WriteLine(sln.MaximumSwap(9973));
+Console.WriteLine(sln.MaximumSwap(98368));
+Console.WriteLine(sln.MaximumSwap(19931105));
+
+public class Solution {
+    public int MaximumSwap(int num) {
+        int best = int.MinValue;
+        char[] digits = num.ToString().ToCharArray();
+
+        for(int i = 0; i < digits.Length; i++)
+        {
+            for (int j = i + 1; j < digits.Length; j++)
+            {
+                (digits[i], digits[j]) = (digits[j], digits[i]);
+                int candidate = int.Parse(new string(digits));
+                best = Math.Max(best, candidate);
+                (digits[i], digits[j]) = (digits[j], digits[i]);
+            }
+        }
+        
+        return best;
+    }
+}
